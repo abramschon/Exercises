cf1a780effe17f8fa623866faf6f917963c1bd28
Abram Schonfeldt <abram.schonfeldt@gmail.com>
2021-05-21 12:11:28 +0200

    Writes names to file

--- 12_RandomMark/RandomMark.py
+++ 12_RandomMark/RandomMark.py
@@ -5,14 +5,10 @@ students = [
     "Samuel",
     "Ruan",
     "Roland",
-    "Nur-Ayn",
     "Nicky",
     "Sergio",
     "William",
-    "Rahul",
-    "Sarah",
     "Georgi",
-    "Grant",
     "Kabelo",
     "Gabriel",
     "Justin",
@@ -22,8 +18,10 @@ students = [
 random.shuffle(students) #randomise order of students
 
 n = 2 # By default select 2 students
-
 if len(sys.argv) == 2:
     n = int(sys.argv[1]) # In case one wants to mark more than 2
 
-print("Students to mark:",students[:n]) 
+with open("who_to_mark.txt", "a") as f:
+    for tut in range(int(len(students)/n)):
+        f.write(f"Tut {tut+1}\n {students[tut*n:tut*n+n]}\n") 
+        
